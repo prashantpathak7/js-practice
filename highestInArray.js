c51d92751439f258cc322905ff6a0e4c62604d32
prashant pathak <prashant209997@gmail.com>
2021-12-30 02:39:15 +0530

    refactoring code

--- highestInArray.js
+++ highestInArray.js
@@ -1,10 +1,10 @@
-const givenArray = [64, 23, 1, 10, 46];
-let highestInArray = 0;
-for (i = 0; i < givenArray.length; i++) {
-  for (j = 0; j < givenArray.length; j++) {
-    if (givenArray[i] < givenArray[j]) {
-      highestInArray = givenArray[j];
+function checkmax(givenArray) {
+  let max = givenArray[0];
+  for (i = 1; i < givenArray.length; i++) {
+    if (max < givenArray[i]) {
+      max = givenArray[i];
     }
   }
+  return max;
 }
-console.log(`Highest Number in given array is ${highestInArray}`);
+console.log(checkmax([64, 53, 98, 10, 5, 96]));
