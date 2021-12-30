53f2f51d62863587f70f202ad7083d0657d1f97f
prashant pathak <prashant209997@gmail.com>
2021-12-31 04:06:30 +0530

    Refactoring code

--- sumOfArray.js
+++ sumOfArray.js
@@ -1,6 +1,9 @@
-const givenArray = [1, 2, 3, 4, 6, 7, 8, 9, 10];
-let sum = 0;
-for (i = 0; i < givenArray.length; i++) {
-  sum = sum + givenArray[i];
+function sumOfArray(givenArray) {
+  let sum = 0;
+  for (i = 0; i < givenArray.length; i++) {
+    sum = sum + givenArray[i];
+  }
+  return sum;
 }
-console.log(sum);
+
+console.log(sumOfArray([1, 10, 4]));
