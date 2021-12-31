41365f1bd95d57db1b42d7a23fd8769d8948b79b
prashant pathak <prashant209997@gmail.com>
2022-01-01 02:37:32 +0530

    refactoring code

--- checkPrimeNumber.js
+++ checkPrimeNumber.js
@@ -1,8 +1,16 @@
-const givenNumber = 124687;
-let a = 'a Prime';
-for (i = 3; givenNumber > i; i++) {
-  if (givenNumber % i == 0) {
-    a = 'not a Prime';
+function isPrime(a) {
+  if (a == 1) {
+    return false;
+  } else if (a == 2 || a == 3) {
+    return true;
+  } else if (a > 3) {
+    for (i = 2; i * i <= a; i++) {
+      if (a % i == 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 }
-console.log(`${givenNumber} is ${a} number`);
+console.log(isPrime(97));
