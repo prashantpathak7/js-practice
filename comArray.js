61fea4d7f15bdd44ada0a2666fc5e26e3d148f9a
prashant pathak <prashant209997@gmail.com>
2022-01-12 21:55:52 +0530

    assignment: compare two arrays using single for loop

---
+++ comArray.js
@@ -0,0 +1,16 @@
+//comapre 2 array using single for loop
+// check whether element of second array are square of first array elements or not using single for loop
+
+function same(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (i = 0; i < a.length; i++) {
+    let c = b.indexOf(a[i] ** 2);
+    if (c === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(same([1, 2, 4], [16, 1, 4]));
